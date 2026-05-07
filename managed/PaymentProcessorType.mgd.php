9b0b2b35b2eaf3fc83d5015d1d1ee03c10e2c519
Sachin Doijad <sachin@skvare.com>
2026-05-07 12:28:23 +0530

    synced the forked branch

--- managed/PaymentProcessorType.mgd.php
+++ managed/PaymentProcessorType.mgd.php
@@ -23,7 +23,7 @@ return [
       'signature_label' => 'Merchant Number',
 
       // Payment class that handles the integration
-      'class_name' => 'Payment_BPoint',
+      'class_name' => 'BPoint',
 
       // Default URLs (can be overridden in processor config)
       'url_site_default' => 'https://www.bpoint.com.au',
